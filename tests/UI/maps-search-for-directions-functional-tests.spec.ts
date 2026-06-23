b68796ed9d34b733d746fdd00924aae2d72ae4fc
maria2709 <37977707+maria2709@users.noreply.github.com>
2026-06-23 11:27:00 +0200

    Update maps-search-for-directions-functional-tests.spec.ts

--- tests/UI/maps-search-for-directions-functional-tests.spec.ts
+++ tests/UI/maps-search-for-directions-functional-tests.spec.ts
@@ -41,7 +41,7 @@ test.describe('Google Maps Feature Tests', () => {
     test(`Validate directions to location ${location}`, { tag: ['@search', '@positive'] }, async ({ page }) => {
 
       await googleMapsPage.searchForLocation(location);
-      await expect(googleMapsPage.getFirstSearchLocationSuggestion()).toContainText(location, { ignoreCase: true });
+      expect(await googleMapsPage.getFirstSearchLocationSuggestion()).toContain(location, { ignoreCase: true });
       await googleMapsPage.selectFirstLocation();
       expect(await googleMapsPage.isDirectionsButtonVisible()).toBeTruthy();
       await googleMapsPage.getDirections();
